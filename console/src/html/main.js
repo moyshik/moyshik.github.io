fc8e46424497c06f4962871ffb67abceec2a8a98
Talat Mahmud Oyshik <59524291+moyshik@users.noreply.github.com>
2020-08-07 17:34:01 +0600

    Create main.js

---
+++ console/src/html/main.js
@@ -0,0 +1,29 @@
+var html = [
+    {
+        id: "000000",
+        title: "Intro to HTML",
+        lessons: [
+            {
+                name: "Foreign Girl",
+                question: false,
+                text: "Imagine you have met a Foreign Girl. Now you asked her out.",
+                code: false
+            },
+            {
+                name: "Her reply",
+                question: {
+                    name: "Guess what she replied",
+                    options: [ // max 5
+                        "Sorry! You\'re not my type",
+                        "Go Fuck somewhere else !!",
+                        "Talk to my dad",
+                        "BDB DS DCU CDAS DUAC YS"
+                    ],
+                    correct: 3
+                },
+                text: false,
+                code: false
+            }
+        ]
+    }
+];
