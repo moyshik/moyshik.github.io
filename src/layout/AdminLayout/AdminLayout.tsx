3a0b8d46c1057ec756c137cdea39c4cc0bde0ad6
Oyshik <moyshik7@gmail.com>
2023-10-29 12:51:03 +0600

    Added favicon and seo

--- src/layout/AdminLayout/AdminLayout.tsx
+++ src/layout/AdminLayout/AdminLayout.tsx
@@ -31,7 +31,35 @@ export default function AdminLayout({ children }: PropsWithChildren) {
       <Head>
         <title>Oyshik</title>
         <meta name="description" content="Stealer of ur mom" />
+
+        <meta property="og:title" content="Oyshik" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://moyshik.github.io" />
+        <meta property="og:image" content="https://moyshik.github.io/i/cover.webp" />
+        <meta name="twitter:title" content="Stealer of ur mom"/>
+        <meta name="twitter:description" content="Sup niggers?"/>
+        <meta name="twitter:image" content="https://moyshik.github.io/i/cover.webp"/>
+        <meta name="twitter:card" content="summary_large_image"/>
+        <meta property="og:site_name" content="Oyshik"/>
+        <meta name="twitter:image:alt" content="Oishik sobar mayre sude"/>
         <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png"/>
+        <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png"/>
+        <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png"/>
+        <link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png"/>
+        <link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png"/>
+        <link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png"/>
+        <link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png"/>
+        <link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png"/>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png"/>
+        <link rel="icon" type="image/png" sizes="192x192"  href="/android-icon-192x192.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+        <link rel="manifest" href="/manifest.json"/>
+        <meta name="msapplication-TileColor" content="#ff6f61"/>
+        <meta name="msapplication-TileImage" content="/ms-icon-144x144.png"/>
+        <meta name="theme-color" content="#ff6f61"/>
       </Head>
 
       <Sidebar isShow={isShowSidebar} isShowMd={isShowSidebarMd} />
